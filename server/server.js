4b6d9df028918d0e4c3a07b8c88dbdd4fa7d3ef8
elilabourne <elilabourne.detorres@gmail.com>
2022-09-06 13:13:12 +0800

    server boilerplate

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    console.log(err.name);
+    console.log(err.code);
+  
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+});
+  
+// Listen on port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
